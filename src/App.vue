dba932167f4d776e7dbc53f968b8c483d411fe9e
Ram Pun <rampun@Rams-MacBook-Pro.local>
2023-04-12 18:44:58 +0800

    add - about page and routers

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div class="container">
     <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask = "showAddTask"/>
-    <div v-if="showAddTask">
-      <AddTask @add-task="addTask"/>
-    </div>
-    <Tasks :tasks="tasks" @toggle-reminder="toggleReminder" @delete-task="deleteTask"/>
+   
+    <router-view :showAddTask="showAddTask"></router-view>
+    
+    <Footer />
   </div>
 </template>
 
 <script>
-import AddTask from './components/AddTask.vue';
+
 import Header from './components/Header.vue';
-import Tasks from './components/Tasks.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks,
-    AddTask,
+    Footer,
   },
   data(){
     return {
-      tasks:[],
       showAddTask: false
     }
   },
@@ -30,69 +28,7 @@ export default {
     toggleAddTask(){
   this.showAddTask = !this.showAddTask
     },
-    async addTask(task)
-    {
-      const res = await fetch('http://127.0.0.1:5000/tasks', {
-        method: 'POST',
-        headers: {
-          'Content-type':'application/json',
-        },
-        body: JSON.stringify(task)
-      })
-      
-      const data = await res.json()
-      this.tasks = [...this.tasks, data]
-    },
-
-    async deleteTask(id){
-      if(confirm('Are you sure?')){
-        const res = await fetch(`http://127.0.0.1:5000/tasks/${id}`, {
-          method: 'DELETE'
-        })
-
-        res.status===  200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
-      }
-    },
-
-    async toggleReminder(id){
-
-      const taskToToggle = await this.fetchTask(id)
-      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
-
-      const res = await fetch(`http://127.0.0.1:5000/tasks/${id}`, {
-          method: 'PUT',
-          headers: {
-          'Content-type':'application/json',
-          },
-          body: JSON.stringify(updTask)
-        })
-
-      const data = await res.json()
-
-      this.tasks = this.tasks.map((task) => 
-        task.id === id ? { ...task, reminder: data.reminder} : task
-      )
-    },
-
-    async fetchTasks(){
-      const res = await fetch('http://127.0.0.1:5000/tasks')
-
-      const data = await res.json()
-
-      return data
-    },
-
-    async fetchTask(id){
-      const res = await fetch(`http://127.0.0.1:5000/tasks/${id}`)
-
-      const data = await res.json()
-
-      return data
-    }
   },
-  async created() {
-    this.tasks = await this.fetchTasks()
-  }
 }
 </script>
 
---
+++ src/views/Home.vue
@@ -0,0 +1,90 @@
+<template>
+    <AddTask v-show="showAddTask" @add-task="addTask"/>
+    <Tasks :tasks="tasks" @toggle-reminder="toggleReminder" @delete-task="deleteTask"/>
+</template>
+
+<script>
+
+import AddTask from '../components/AddTask.vue';
+import Tasks from '../components/Tasks.vue';
+
+export default {
+    name: 'Home',
+    props: {
+        showAddTask: Boolean
+    },
+    components: {
+        Tasks,
+        AddTask
+    },
+    data(){
+    return {
+      tasks:[],
+    }
+  },
+  methods:{
+    async addTask(task)
+    {
+      const res = await fetch('http://127.0.0.1:5000/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type':'application/json',
+        },
+        body: JSON.stringify(task)
+      })
+      
+      const data = await res.json()
+      this.tasks = [...this.tasks, data]
+    },
+
+    async deleteTask(id){
+      if(confirm('Are you sure?')){
+        const res = await fetch(`http://127.0.0.1:5000/tasks/${id}`, {
+          method: 'DELETE'
+        })
+
+        res.status===  200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
+      }
+    },
+
+    async toggleReminder(id){
+
+      const taskToToggle = await this.fetchTask(id)
+      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
+
+      const res = await fetch(`http://127.0.0.1:5000/tasks/${id}`, {
+          method: 'PUT',
+          headers: {
+          'Content-type':'application/json',
+          },
+          body: JSON.stringify(updTask)
+        })
+
+      const data = await res.json()
+
+      this.tasks = this.tasks.map((task) => 
+        task.id === id ? { ...task, reminder: data.reminder} : task
+      )
+    },
+
+    async fetchTasks(){
+      const res = await fetch('http://127.0.0.1:5000/tasks')
+
+      const data = await res.json()
+
+      return data
+    },
+
+    async fetchTask(id){
+      const res = await fetch(`http://127.0.0.1:5000/tasks/${id}`)
+
+      const data = await res.json()
+
+      return data
+    }
+  },
+  async created() {
+    this.tasks = await this.fetchTasks()
+  }
+}
+</script>
